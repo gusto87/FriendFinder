0d935ee3ee1337a457f749fc1dfd83ae1ae8cfa2
will <wsherrill17@gmail.com>
2019-08-19 12:18:47 -0400

    finished functions to get app working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // npm packages used for the server
 var express = require("express");
-
+var bodyparser = require('body-parser')
+var path = require('path')
 
 //lets node know we are creating an express server
 var app = express();
@@ -11,6 +12,8 @@ var PORT = process.env.PORT || 8080;
 // sets the express app to handle data parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(bodyparser.text());
+app.use(bodyparser.json({type: 'application/vnd.api+json'}));
 
 // routes to direct the server
 require("./app/routing/apiRoutes")(app);
